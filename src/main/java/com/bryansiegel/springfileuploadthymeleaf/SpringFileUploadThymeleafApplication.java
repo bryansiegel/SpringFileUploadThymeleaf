9a7baf9b3f4f1c67ac001e7a2abf7c3a49865828
Bryan Siegel <bryansiegel@gmail.com>
2024-05-15 14:16:41 -0700

    initial commit

---
+++ src/main/java/com/bryansiegel/springfileuploadthymeleaf/SpringFileUploadThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.springfileuploadthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileUploadThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileUploadThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bryansiegel/springfileuploadthymeleaf/SpringFileUploadThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryansiegel.springfileuploadthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFileUploadThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
